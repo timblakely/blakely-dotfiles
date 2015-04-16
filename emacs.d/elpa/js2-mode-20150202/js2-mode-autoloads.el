95140c862da68d19b94644f822d134e66c03a95b
Tim Blakely <tim.blakely@gmail.com>
2015-04-16 11:51:30 -0700

    Oops, forgot emacs.d directory

---
+++ emacs.d/elpa/js2-mode-20150202/js2-mode-autoloads.el
@@ -0,0 +1,56 @@
+;;; js2-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (js2-imenu-extras-mode js2-imenu-extras-setup)
+;;;;;;  "js2-imenu-extras" "../../../../../.emacs.d/elpa/js2-mode-20150202/js2-imenu-extras.el"
+;;;;;;  "a7580f86032a9ca1d6f6e543cf770ba7")
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/js2-mode-20150202/js2-imenu-extras.el
+
+(autoload 'js2-imenu-extras-setup "js2-imenu-extras" "\
+
+
+\(fn)" nil nil)
+
+(autoload 'js2-imenu-extras-mode "js2-imenu-extras" "\
+Toggle Imenu support for frameworks and structural patterns.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads (js2-mode js2-minor-mode) "js2-mode" "../../../../../.emacs.d/elpa/js2-mode-20150202/js2-mode.el"
+;;;;;;  "83e133efb2cbba24d9a4a9dda4dbc549")
+;;; Generated autoloads from ../../../../../.emacs.d/elpa/js2-mode-20150202/js2-mode.el
+
+(autoload 'js2-minor-mode "js2-mode" "\
+Minor mode for running js2 as a background linter.
+This allows you to use a different major mode for JavaScript editing,
+such as `js-mode', while retaining the asynchronous error/warning
+highlighting features of `js2-mode'.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'js2-mode "js2-mode" "\
+Major mode for editing JavaScript code.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/js2-mode-20150202/js2-imenu-extras.el"
+;;;;;;  "../../../../../.emacs.d/elpa/js2-mode-20150202/js2-mode-pkg.el"
+;;;;;;  "../../../../../.emacs.d/elpa/js2-mode-20150202/js2-mode.el")
+;;;;;;  (21799 29015 475518 982000))
+
+;;;***
+
+(provide 'js2-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; js2-mode-autoloads.el ends here
